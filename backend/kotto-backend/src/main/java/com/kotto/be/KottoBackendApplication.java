ac3793087245ddf368d415a27ff6e4c6e538d26b
Yasiru Silva <yasiru017@gmail.com>
2026-02-22 22:28:19 +0530

    Initial project setup - Backend and Frontend initialized

---
+++ backend/kotto-backend/src/main/java/com/kotto/be/KottoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.kotto.be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KottoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KottoBackendApplication.class, args);
+	}
+
+}
---
+++ backend/kotto-backend/src/test/java/com/kotto/be/KottoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kotto.be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KottoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
