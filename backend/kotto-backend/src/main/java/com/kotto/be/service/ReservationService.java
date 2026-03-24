fd46112f455399d4c85300e2965ac451ae74b49c
Yasiru Silva <yasiru017@gmail.com>
2026-03-24 09:01:44 +0530

    Update ReservationService

--- backend/kotto-backend/src/main/java/com/kotto/be/service/ReservationService.java
+++ backend/kotto-backend/src/main/java/com/kotto/be/service/ReservationService.java
@@ -2,7 +2,10 @@ package com.kotto.be.service;
 
 import com.kotto.be.dto.CreateReservationRequest;
 import com.kotto.be.dto.CreateReservationResponse;
+import com.kotto.be.dto.ReservationResponseDto;
+import java.util.List;
 
 public interface ReservationService {
     CreateReservationResponse create(CreateReservationRequest request, String userEmail);
+    List<ReservationResponseDto> getUserReservations(String userEmail);
 }
