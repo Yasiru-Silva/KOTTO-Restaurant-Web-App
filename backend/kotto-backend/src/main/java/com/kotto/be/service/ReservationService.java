33536be3c63e3f2d592fbb8eefa3fef35321af94
Navindu Dileepa <IT24101515@my.sliit.lk>
2026-03-24 23:45:34 +0530

    add admin reservation methods (getAll, approve, reject) to service interface

--- backend/kotto-backend/src/main/java/com/kotto/be/service/ReservationService.java
+++ backend/kotto-backend/src/main/java/com/kotto/be/service/ReservationService.java
@@ -3,9 +3,17 @@ package com.kotto.be.service;
 import com.kotto.be.dto.CreateReservationRequest;
 import com.kotto.be.dto.CreateReservationResponse;
 import com.kotto.be.dto.ReservationResponseDto;
+
 import java.util.List;
 
 public interface ReservationService {
     CreateReservationResponse create(CreateReservationRequest request, String userEmail);
+
     List<ReservationResponseDto> getUserReservations(String userEmail);
+
+    List<ReservationResponseDto> getAllReservations();
+
+    ReservationResponseDto approveReservation(Long reservationId);
+
+    ReservationResponseDto rejectReservation(Long reservationId);
 }
